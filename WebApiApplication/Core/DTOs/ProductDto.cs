ff2226f5335c987e46c92ab9adf166515f4596c6
Jiří Fůsek <jiri.fusek@outlook.com>
2025-03-13 21:29:02 +0100

    Implemented Product dto class

--- WebApiApplication/Core/DTOs/ProductDto.cs
+++ WebApiApplication/Core/DTOs/ProductDto.cs
@@ -8,5 +8,10 @@ namespace Core.DTOs
 {
     public class ProductDto
     {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string ImgUri { get; set; }
+        public decimal Price { get; set; }
+        public string? Description { get; set; }
     }
 }
