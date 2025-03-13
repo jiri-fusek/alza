f9a31db9746888d06adb3cd575cacce78f92aadf
Jiří Fůsek <jiri.fusek@outlook.com>
2025-03-13 21:27:26 +0100

    Implemented Product entity class

--- WebApiApplication/Core/Entities/Product.cs
+++ WebApiApplication/Core/Entities/Product.cs
@@ -8,5 +8,10 @@ namespace Core.Entities
 {
     public class Product
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string ImgUri { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string? Description { get; set; }
     }
 }
