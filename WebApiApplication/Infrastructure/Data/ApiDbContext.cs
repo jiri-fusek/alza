735d166d853dbad7a1b92f54cec683e6af7f3fae
Jiří Fůsek <jiri.fusek@outlook.com>
2025-03-13 21:58:30 +0100

    Implemented ApiDbContext class and added Entity Framework package

---
+++ WebApiApplication/Infrastructure/Data/ApiDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configure Product entity
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Name).IsRequired().HasMaxLength(100);
+                entity.Property(e => e.ImgUri).IsRequired().HasMaxLength(500);
+                entity.Property(e => e.Price).IsRequired().HasColumnType("decimal(18,2)");
+                entity.Property(e => e.Description).HasMaxLength(2000);
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
