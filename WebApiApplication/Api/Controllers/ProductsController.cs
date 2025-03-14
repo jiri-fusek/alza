0fc7a1a81359507deca96a93538967fb813f798a
Jiří Fůsek <jiri.fusek@outlook.com>
2025-03-14 08:42:52 +0100

    Implemented ProductsController class

---
+++ WebApiApplication/Api/Controllers/ProductsController.cs
@@ -0,0 +1,78 @@
+﻿using Core.DTOs;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        /// <summary>
+        /// Retrieves all products
+        /// </summary>
+        /// <returns>A list of all products</returns>
+        /// <response code="200">Returns all products</response>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<ProductDto>>> GetProducts()
+        {
+            var products = await _productService.GetAllProductsAsync();
+            return Ok(products);
+        }
+
+        /// <summary>
+        /// Retrieves a specific product by id
+        /// </summary>
+        /// <param name="id">The id of the product to retrieve</param>
+        /// <returns>The requested product</returns>
+        /// <response code="200">Returns the requested product</response>
+        /// <response code="404">If the product is not found</response>
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<ProductDto>> GetProduct(int id)
+        {
+            var product = await _productService.GetProductByIdAsync(id);
+
+            if (product == null)
+                return NotFound();
+
+            return Ok(product);
+        }
+
+        /// <summary>
+        /// Updates the description of a specific product
+        /// </summary>
+        /// <param name="id">The id of the product to update</param>
+        /// <param name="descriptionDto">The new description</param>
+        /// <returns>No content if successful</returns>
+        /// <response code="204">If the update was successful</response>
+        /// <response code="404">If the product is not found</response>
+        /// <response code="400">If the request is invalid</response>
+        [HttpPatch("{id}/description")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> UpdateProductDescription(int id, [FromBody] UpdateProductDescriptionDto descriptionDto)
+        {
+            if (descriptionDto == null)
+                return BadRequest();
+
+            var result = await _productService.UpdateProductDescriptionAsync(id, descriptionDto);
+
+            if (!result)
+                return NotFound();
+
+            return NoContent();
+        }
+    }
+}
