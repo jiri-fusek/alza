16b55b655096f9467764a629a663064625b9b2d2
Jiří Fůsek <jiri.fusek@outlook.com>
2025-03-14 08:31:40 +0100

    Implemented ProductRepository class

---
+++ WebApiApplication/Infrastructure/Data/ProductRepository.cs
@@ -0,0 +1,51 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApiDbContext _context;
+
+        public ProductRepository(ApiDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Product>> GetAllProductsAsync()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<Product?> GetProductByIdAsync(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+        public async Task<bool> UpdateProductDescriptionAsync(int id, string? description)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+                return false;
+
+            product.Description = description;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch (DbUpdateException)
+            {
+                return false;
+            }
+        }
+    }
+}
